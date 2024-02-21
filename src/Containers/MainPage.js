4939e7dc73cb473b81a48753e4cc2421468d5915
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-02-21 15:12:01 +0000

    made log in page functional

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./MainPage.css"
 
-const MainPage = ({name, weight, selectedAge}) => {
+const MainPage = ({user}) => {
     const navigate = useNavigate();
 
     const handleWorkoutClick = (event) => {
@@ -17,8 +17,8 @@ const MainPage = ({name, weight, selectedAge}) => {
     return(
         <div>
             <div className="top">
-                <h1>Hi {name}</h1>
-                <h1>You weight {weight}kg</h1>
+                <h1>Hi {user.name}</h1>
+                <h1>You weight {user.weight}kg</h1>
             </div>
             <div className="options">
                 <button className="diet" onClick={handleDietClick}>Diet Plans</button>
--- src/Containers/SecondPage.js
+++ src/Containers/SecondPage.js
@@ -7,6 +7,7 @@ import ReactModal from "react-modal";
 const SecondPage = ({user, setUser}) => {
     const navigate = useNavigate();
 
+// Log In Modal
     const customStyles = {
         content: {
           top: '50%',
@@ -15,6 +16,7 @@ const SecondPage = ({user, setUser}) => {
           bottom: 'auto',
           marginRight: '-50%',
           transform: 'translate(-50%, -50%)',
+          background: 'red',
         },
       };
 
@@ -22,28 +24,90 @@ const SecondPage = ({user, setUser}) => {
     const openLogInModal = () => {setLogInModalOpen(true)};
     const closeLogInModal = () => {setLogInModalOpen(false)};
 
+    const handleLogIn = (event) => {
+        event.preventDefault();
+        openLogInModal();
+    }
+
     const [logInEmail, setLogInEmail] = useState("");
     const [logInPassword, setLogInPassword] = useState("");
+    const [hideWrongLogInMessage, setHideWrongLogInMessage] = useState(true);
 
-    const [name, setName] = useState("");
-    const [selectedAge, setSelectedAge] = useState("Empty");
-    const [weight, setWeight] = useState(null);
-
-    const [hideMessage, setHideMessage] = useState(true)
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        if(name!="" && selectedAge!="Empty" && weight!=null){
-            navigate("/MainPage")
-            setHideMessage(true);
+    const postLogInCustomer = async (logInEmail, logInPassword) => {
+        let temp = {
+            email: logInEmail,
+            password: logInPassword
+        }
+        const newResponse = await fetch(`http://localhost:8080/users/authenticate`,{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body:JSON.stringify(temp)
+        })
+        console.log(newResponse.status);
+        if(newResponse.status === 202){
+            const newC = await newResponse.json();
+            setUser(newC);
+            navigate("/MainPage");
+            setLogInEmail("");
+            setLogInPassword("");
+            setHideWrongLogInMessage(true);
         } else {
-            setHideMessage(false);
+            setHideWrongLogInMessage(false);
         }
     }
 
     const handleLogInFormSubmit = (event) => {
         event.preventDefault();
-        // postLogInCustomer(logInEmail,logInPassword);
+        postLogInCustomer(logInEmail,logInPassword);
+    }
+
+// Main Page Stuff
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [weight, setWeight] = useState(null);
+    const [selectedAge, setSelectedAge] = useState("Empty");
+
+    const [hideMessage, setHideMessage] = useState(true)
+    const [hideEmailAlreadyInUse, setHideEmailAlreadyInUse] = useState(true);
+
+    const postRegisterUser = async (name, email, password, weight, selectedAge) => {
+        let temp = {
+            name: name,
+            email: email,
+            password: password,
+            weight: weight,
+            selectedAge: selectedAge
+        }
+        const newResponse = await fetch(`http://localhost:8080/users`,{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body:JSON.stringify(temp)
+        })
+        if(newResponse.status === 202){
+            const newR = await newResponse.json();
+            setUser(newR);
+            setHideEmailAlreadyInUse(true);
+            setName("");
+            setEmail("");
+            setPassword("");
+            setWeight(null);
+            setSelectedAge("Empty");
+            navigate("/MainPage");
+        } else {
+            setHideEmailAlreadyInUse(false);
+        }
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        if(name!="" && selectedAge!="Empty" && weight!=null){
+            postRegisterUser(name,email,password,weight,selectedAge);
+            // navigate("/MainPage")
+            setHideMessage(true);
+        } else {
+            setHideMessage(false);
+        }
     }
 
     const handleDropdownChange = (event) => {
@@ -51,37 +115,34 @@ const SecondPage = ({user, setUser}) => {
         setSelectedAge(event.target.value)
     }
 
-    const handleLogIn = (event) => {
-        event.preventDefault();
-    }
-
     return(
         <div className="second-main">
             <Modal
-                    isOpen={logInModalOpen}
-                    onRequestClose={closeLogInModal}
-                    style={customStyles}
-                    contentLabel="Example Modal"
-                    ariaHideApp={false}
-                >
-                    <h1>Enter Your Details</h1>
-                    <form onSubmit={handleLogInFormSubmit} className="log-in">
-                        <input type="text" 
-                        name="email"
-                        placeholder="Email"
-                        value={logInEmail}
-                        onChange={(e) => setLogInEmail(e.target.value)}
-                        />
-                        <input type="password" 
-                        name="password"
-                        placeholder="Password"
-                        value={logInPassword}
-                        onChange={(e) => setLogInPassword(e.target.value)}
-                        />
-                        <button type="submit">Log In</button>
-                        {/* <p hidden={hideWrongLogInMessage} style={{color:"red"}}>Wrong Credentials</p> */}
-                    </form>
-                </Modal>
+                isOpen={logInModalOpen}
+                onRequestClose={closeLogInModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+                ariaHideApp={false}
+                
+            >
+                {/* <h1>Enter Your Details</h1> */}
+                <form onSubmit={handleLogInFormSubmit} className="log-in-form">
+                    <input type="text" 
+                    name="email"
+                    placeholder="Email"
+                    value={logInEmail}
+                    onChange={(e) => setLogInEmail(e.target.value)}
+                    />
+                    <input type="password" 
+                    name="password"
+                    placeholder="Password"
+                    value={logInPassword}
+                    onChange={(e) => setLogInPassword(e.target.value)}
+                    />
+                    <button type="submit">Log In</button>
+                    <p hidden={hideWrongLogInMessage} style={{color:"black"}}>Wrong Credentials</p>
+                </form>
+            </Modal>
 
 
             <div className="log-in">
@@ -90,11 +151,27 @@ const SecondPage = ({user, setUser}) => {
             <div className="main-info">
                 <h1>Tell me a bit about yourself</h1>
                 <form onSubmit={handleFormSubmit} className="form">
+                    <p hidden={hideMessage}>Please fill in all boxes</p>
+                    <p hidden={hideEmailAlreadyInUse}>Email already in use</p>
                     <input type="text" 
                     name="name"
                     placeholder="Name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
+                    className="inputs"
+                    />
+                    <input type="text" 
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="inputs"
+                    />
+                    <input type="text" 
+                    name="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     className="inputs"
                     />
                     <input type="integer" 
@@ -117,7 +194,7 @@ const SecondPage = ({user, setUser}) => {
                         <option value={"75+"}>75+</option>
                     </select>
 
-                    <p hidden={hideMessage}>Please fill in all boxes</p>
+                    
                     <button type="submit" className="btn-form">Lets Excercise</button>
                 </form>
             </div>
