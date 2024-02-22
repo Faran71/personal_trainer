8c5e6fe4af3487113fb546fe7c75a52bd1c53e4e
Muhammad Faran Sarwar <muhammadfaran01.mfs@gmail.com>
2024-02-22 21:08:43 +0000

    added external api but the recipe api doesnt seem to respond, need to change the api

--- src/Containers/DietPlan.js
+++ src/Containers/DietPlan.js
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 const DietPlan = () => {
 
-    return(
-        <div>
-            <h1>Diet</h1>
+    const [search, setSearch] = useState("");
+    const [recipe, setRecipe] = useState(null);
 
-            {/* 
-            
-            Add API from internet
-            
-            */}
-        </div>
-    )
+    // Not working
+    const getRecipe = async (search) => {
+        const newResponse = await fetch(`https://api.calorieninjas.com/v1/recipe?query=${search}`,{
+            method: "GET",
+            headers: {'X-Api-Key':'EoE9Vfi9YlG4Q7OHKPYGOg==iDoP16NR6Snt1UNx',
+            "Content-Type": "application/json"}
+        })
+        const newR = await newResponse.json();
+        setRecipe(newR);
+    }
+
+    const handleClick = (event) => {
+        event.precentDefault();
+        getRecipe("mushroom risotto");
+    }
+
+    if(recipe){
+        return(
+            <div>
+                <button onClick={handleClick}>Click</button>
+                <p>{recipe[0].title}</p>
+            </div>
+        )
+    } else {
+        return(
+            <div>
+                <button onCLick={handleClick}>Click</button>
+                <h1>No Recipe</h1>
+            </div>
+        )
+    }
+    
 
 }
 
